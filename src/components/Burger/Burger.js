1a05907cb55c3289486e1d7a04aaca0ed09f72b6
Thomas Hutchinson <thomashutchinson450@gmail.com>
2020-09-25 11:06:59 +0100

    added modules and created dynamic Burger component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import classes from "./Burger.module.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-const Burger = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Burger = ({ ingredients }) => {
+  const transformedIngredients = Object.keys(ingredients)
+  .map((igKey)=> {
+      return [...Array(ingredients[igKey])].map((_, i) =>{
+          return <BurgerIngredient key={igKey+i} type={igKey}/>
+      });
+  });
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
 
-export default Burger
+export default Burger;
